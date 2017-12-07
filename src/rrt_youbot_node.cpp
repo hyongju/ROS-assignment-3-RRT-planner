981f6c0ca3facbe08a94603735c753b7149699df
Hyongju Park <hjcpark@umich.edu>
2017-12-07 13:17:53 -0500

    Update rrt_youbot_node.cpp

--- src/rrt_youbot_node.cpp
+++ src/rrt_youbot_node.cpp
@@ -10,13 +10,13 @@ int main(int argc, char **argv)
 	srand( time(0));
 
 	// initialize as a ROS program
-    ros::init(argc, argv,"rrt_youbot_node",ros::init_options::NoSigintHandler);  
-    
-    // create node handle
-    ros::NodeHandle n;	
-    
-    // create motionPlanning object with the node handler
-    motionPlanning mp(n);
-    
+	ros::init(argc, argv,"rrt_youbot_node",ros::init_options::NoSigintHandler);  
+
+	// create node handle
+	ros::NodeHandle n;	
+
+	// create motionPlanning object with the node handler
+	motionPlanning mp(n);
+
 	return 0;
 }
